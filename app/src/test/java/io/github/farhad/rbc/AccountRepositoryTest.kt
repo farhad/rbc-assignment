9c8833545d777b1ccbdbc1883d9bf59264d0913b
Farhad Faghihi <ifarhadfaghihi@gmail.com>
2022-03-14 01:26:06 -0600

    added two more test cases to account repository

--- app/src/test/java/io/github/farhad/rbc/AccountRepositoryTest.kt
+++ app/src/test/java/io/github/farhad/rbc/AccountRepositoryTest.kt
@@ -110,6 +110,82 @@ class AccountRepositoryTest {
     }
 
     @Test
+    fun when_account_type_is_not_credit_card_it_does_not_call_provider_getAdditionalCreditCardTransactions() = runBlocking {
+        // arrange
+        val account = newAccount("test-account-one", "2323", "100.23", AccountType.MORTGAGE)
+        val transaction = newTransaction("test-transaction", "23.56", Calendar.getInstance())
+
+        class MockedAccountDataProvider : AccountDataProvider {
+            override suspend fun getAccounts(): List<Account> {
+                return listOf(account)
+            }
+
+            override suspend fun getAccountTransactions(accountNumber: String): List<Transaction> {
+                return listOf(transaction)
+            }
+
+            override suspend fun getAdditionalCreditCardTransactions(accountNumber: String): List<Transaction> {
+                return listOf(newTransaction("transaction-two", "78.16", Calendar.getInstance()))
+            }
+        }
+
+        val mockedProvider = MockedAccountDataProvider()
+        val repository = AccountRepositoryImpl(mockedProvider)
+
+        // act
+        val result = repository.getTransactionsAsync(account.number, account.type).await()
+
+        // assert
+        Assert.assertTrue(result is Result.Success<Transaction>)
+        result as Result.Success<Transaction>
+        Assert.assertTrue(result.data.isNotEmpty())
+        Assert.assertTrue(result.data.size == 1)
+        Assert.assertTrue(result.data[0] == transaction)
+    }
+
+    @Test
+    fun when_account_type_is_credit_card_it_calls_both_provider_getTransaction_methods() = runBlocking {
+        // arrange
+        val account = newAccount("test-account-one", "2323", "100.23", AccountType.CREDIT_CARD)
+
+        val dayOne = Calendar.getInstance()
+        dayOne.set(2022, 1, 2, 12, 2)
+        val transactionOne = newTransaction("transaction-one", "23.56", dayOne)
+
+        val dayTwo = Calendar.getInstance()
+        dayTwo.set(2022, 1, 3, 12, 45)
+        val transactionTwo = newTransaction("transaction-two", "78.16", dayTwo)
+
+        class MockedAccountDataProvider : AccountDataProvider {
+            override suspend fun getAccounts(): List<Account> {
+                return listOf(account)
+            }
+
+            override suspend fun getAccountTransactions(accountNumber: String): List<Transaction> {
+                return listOf(transactionOne)
+            }
+
+            override suspend fun getAdditionalCreditCardTransactions(accountNumber: String): List<Transaction> {
+                return listOf(transactionTwo)
+            }
+        }
+
+        val mockedProvider = MockedAccountDataProvider()
+        val repository = AccountRepositoryImpl(mockedProvider)
+
+        // act
+        val result = repository.getTransactionsAsync(account.number, account.type).await()
+
+        // assert
+        Assert.assertTrue(result is Result.Success<Transaction>)
+        result as Result.Success<Transaction>
+        Assert.assertTrue(result.data.isNotEmpty())
+        Assert.assertTrue(result.data.size == 2)
+        Assert.assertTrue(result.data[0] == transactionTwo)
+        Assert.assertTrue(result.data[1] == transactionOne)
+    }
+
+    @Test
     fun it_sorts_transactions_from_provider_getTransactions_combined_result_descending_by_date() = runBlocking {
         // arrange
         val account = newAccount("test-account-one", "2323", "100.23", AccountType.CREDIT_CARD)
